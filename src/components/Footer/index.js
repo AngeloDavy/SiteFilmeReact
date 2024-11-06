f24d94060654ec24c0e6980bf5c11c30d1bf5880
angelo <angeloportela2@gmail.com>
2024-11-06 02:06:56 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <h2>MaiaFlix &copy; Desenvolvido por Angelo Davy 2024</h2>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
