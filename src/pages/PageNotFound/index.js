5b7d7e314720de596f1561189f98e09db3f535d5
AngeloDavy <angeloportela2@gmail.com>
2024-11-10 23:51:40 -0300

    att

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,13 +1,17 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 import styles from "./PageNotFound.module.css"
 import erro404 from './erro404.png'
 
 function PageNotFound(){
     return(
         <>
+        <Header />
             <section className={styles.container}>
                 <h2>Opss! Conteúdo não encotrado!</h2>
                 <img src={erro404} alt="Logo de página não localizada"/>
             </section>
+        <Footer />
         </>
     )
 }
--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import styles from "./Watch.module.css";
+import videos from "../../json/videos.json"
+import PageNotFound from "../PageNotFound"
 
 function Watch() {
+
+  const params = useParams()
+  const video = videos.find((video) => { return video.id === params.id })
+  if(!video){ return <PageNotFound /> }
+
   return (
     <>
       <Header />
@@ -12,7 +20,7 @@ function Watch() {
           <iframe
             width="854"
             height="480"
-            src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=hBr7vne7PZWIvszE"
+            src={`https://www.youtube.com/embed/${video.id}`}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
