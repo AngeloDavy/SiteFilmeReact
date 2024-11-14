2577e7e0e68cdfe173171e10cf1f4554a8aeed85
AngeloDavy <angeloportela2@gmail.com>
2024-11-13 23:40:27 -0300

    useState-aula

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
@@ -5,12 +6,27 @@ import Header from "../../components/Header";
 import CategoryMap from "../../components/RepetCategory";
 
 function Home() {
+
+  function pegarNome(e){
+    setNome(e.target.value)
+  }
+
+  const [nome, setNome] = useState()
+
   return (
     <div>
       <Header />
       <Banner image="home" />
       <Container>
-        <CategoryMap />
+
+      <input 
+        type="text"
+        placeholder="Nome"
+        onChange={pegarNome}
+      />
+      <h2>{nome}</h2>
+
+        {/*<CategoryMap />*/}
       </Container>
       <Footer />
     </div>
