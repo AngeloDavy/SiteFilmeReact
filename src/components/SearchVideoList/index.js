7f0fc8593cff4f9a09cb0ddcebf9398b177b067b
AngeloDavy <angeloportela2@gmail.com>
2024-11-17 01:32:43 -0300

    att filtro pag pesquisar

---
+++ src/components/SearchVideoList/index.js
@@ -0,0 +1,31 @@
+import styles from "./SearchVideoList.module.css"
+import VideoList from "../../components/VideoList"
+import { useState } from "react";
+
+function filterVideos(videos, searchText){
+    return videos.filter(
+        (video) => video.category.includes(searchText) || video.title.includes(searchText)
+    )
+}
+
+function SearchVideoList({ videos }){
+
+    const [searchText, setSearchText ] = useState("Geografia")
+    const foundVideos = filterVideos(videos, searchText)
+
+    return(
+        <>
+            <section className={styles.container}>
+                <input
+                    type="search"
+                    placeholder="Pesquisar..."
+                    value={searchText}
+                    onChange={e => setSearchText(e.target.value)}
+                />
+                <VideoList videos={foundVideos} empyHeading={`Sem vídeos sobre "${searchText}"`}/>
+            </section> 
+        </>
+    )
+}
+
+export default SearchVideoList;
